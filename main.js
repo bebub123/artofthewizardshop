f4a90ba8d4d9f6a4da80f5042bbfbcbd5995df63
bebub123 <88442713+bebub123@users.noreply.github.com>
2025-07-20 03:11:51 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-import { productBanners, createBannerHtml } from './products.js';
-
-const container = document.querySelector('#banner-container');
-
-productBanners.forEach(banner => {
-  container.innerHTML += createBannerHtml(banner);
-});
